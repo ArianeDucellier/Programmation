7954dd9ad0231549befaca0561e85754b2d018d0
ArianeDucellier <ariane.ducellier.pro@gmail.com>
2020-02-14 15:12:53 -0800

    Started BC ex5.2

--- DTW/simple_DTW.py
+++ DTW/simple_DTW.py
@@ -1,23 +1,37 @@
-""" Naive method to do Dynamic Time Warping """
+""" Blanchet and Charbit """
 
 import numpy as np
 
-def DWT_vector(X, Y):
+def ex_5_2(X, Y):
     """
     """
-    assert np.shape(X)[0] < np.shape(Y)[Y], \
-        'The first vector must be smaller than the second vector'
-    n = np.shape(X)[0]
-    p = np.shape(Y)[0]
-    ix0 = 
-    
-def DWT_step_vector(X, Y):
-    """
-    """
-    n = np.shape(X)[0]
-    p = np.shape(Y)[0]
-    L = n - p
-    distance = np.zeros(L)
-    for l in range(0, L + 1):
-        Xl = np.concatenate(np.repeat(X[0], l + 1), \
-            X[1:-1], np.repeat(X[-1], L - l + 1))
+    I = np.shape(X)[0]
+    J = np.shape(Y)[0]
+    d = np.zeros((I, J))
+    for i in range(0, I):
+        for j in range(0, J):
+            d[i, j] = np.abs(X[i] - Y[j])
+    D = np.zeros((I, J))
+    D[0, 0] = d[0, 0]
+    for k in range(1, min(I, J)):
+        for i in range(0, k):
+            if (i == 0):
+                D[i, k] = D[i, k - 1] + d[i, k]
+            else:
+                path1 = D[i - 1, k] + d[i, k]
+                path2 = D[i - 1, k - 1] + 2.0 * d[i, k]
+                path3 = D[i, k - 1] + d[i, k]
+                D[i, k] = min(path1, path2, path3)
+        for j in range(0, k):
+            if (j == 0):
+                D[k, j] = D[k - 1, j] + d[k, j]
+            else:
+                path1 = D[k, j - 1] + d[k, j]
+                path2 = [k - 1, j - 1] + 2.0 * d[k, j]
+                path3 = D[k - 1, j] + d[k, j]
+                D[k, j] = min(path1, path2, path3)
+        path1 = D[k - 1, k] + d[k, k]
+        path2 = D[k - 1, k - 1] + 2.0 * d[k, k]
+        path3 = D[k, k - 1] + d[k, k]
+        D[k, k] = min(path1, path2, path3)
+    return D
