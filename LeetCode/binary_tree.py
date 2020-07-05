d69d366d6c779233040f268c9a6975719c7d5627
ArianeDucellier <ariane.ducellier.pro@gmail.com>
2020-07-04 18:05:16 -0700

    binary tree

--- LeetCode/binary_tree.py
+++ LeetCode/binary_tree.py
@@ -1,9 +1,9 @@
 # To build tree nodes
 class TreeNode(object):
-     def __init__(self, x):
-         self.val = x
-         self.left = None
-         self.right = None
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
 
 class Solution(object):
 
@@ -30,7 +30,6 @@ class Solution(object):
         
         return output
 
-    # Second version with space complexity O(1)
     def preorderTraversal_2(self, root):
         """
         :type root: TreeNode
@@ -197,128 +196,244 @@ class Solution(object):
                 isright = self.hasPathSum(root.right, sum - root.val)
             return (isleft or isright)
 
+    # Auxiliary function for countUnivalSubtrees
+    def isUnivalSubtrees(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root == None:
+            return (0, False)
+        
+        if (root.right == None) and (root.left == None):
+            return (1, True)
+
+        isLeft = False
+        if (root.left == None):
+            nb_left = 0
+            isLeft = True
+        else:
+            (nb_left, univalue) = self.isUnivalSubtrees(root.left)
+            if (root.left.val == root.val) and (univalue == True):
+                isLeft = True
+
+        isRight = False
+        if (root.right == None):
+            nb_right = 0
+            isRight = True
+        else:
+            (nb_right, univalue) = self.isUnivalSubtrees(root.right)
+            if (root.right.val == root.val) and (univalue == True):
+                isRight = True
+
+        if (isLeft == True) and (isRight == True):
+            return (nb_left + nb_right + 1, True)
+        else:
+            return (nb_left + nb_right, False)
+
     # Count the number of uni-value subtrees
     def countUnivalSubtrees(self, root):
         """
         :type root: TreeNode
         :rtype: int
         """
-        if (root.right == None) and (root.left == None):
-            return 1
-        else:
-            isLeft = False
-            isRight = False
-            if (root.left == None):
-                nb_left = 0
-                isLeft = True
-            else:
-                nb_left = self.countUnivalSubtrees(root.left)
-                if (root.left.val == root.val):
-                    isLeft = True
-            if (root.right == None):
-                nb_right = 0
-                isRight = True
-            else:
-                nb_right = self.countUnivalSubtrees(root.right)
-                if (root.right.val == root.val):
-                    isRight = True
-            if (isLeft == True) and (isRight == True):
-                return nb_left + nb_right + 1
-            else:
-                return nb_left + nb_right
+        (nb, is_subtree) = self.isUnivalSubtrees(root)
+        return nb
 
-if __name__ == '__main__':
+    # Get tree for inorder and postorder traversal
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if (inorder == []) or (postorder == []):
+            return None
+        val = None
+        while val not in inorder:
+            val = postorder.pop()
+        index = inorder.index(val)
+        root = TreeNode(val) 
+        inorder_left = inorder[0 : index]
+        if inorder_left:
+            root.left = self.buildTree(inorder_left, postorder.copy())
+        inorder_right = inorder[index + 1 : ]
+        if inorder_right:
+            root.right = self.buildTree(inorder_right, postorder.copy())
+        return root
 
+def test_preorderTraversal_1():
+    """
+    Input: [1,null,2,3]
+    Output: [1,2,3]
+    """
     s = Solution()
+    three = TreeNode(3)
+    two = TreeNode(2)
+    one = TreeNode(1)
+    two.left = three
+    one.right = two
+    result = s.preorderTraversal_1(one)
+    print('Preorder traversal:')
+    print(result)
+    print('\n')
 
-#    three = TreeNode(3)
-#    two = TreeNode(2)
-#    one = TreeNode(1)
-#    two.left = three
-#    one.right = two
-#    result_1 = s.preorderTraversal_1(one)
-#    print('Preorder traversal:')
-#    print(result_1)
-#    print('\n')
-#    result_2 = s.preorderTraversal_2(one)
-#    print('Preorder traversal:')
-#    print(result_2)
-#    print('\n')
-#    result = s.inorderTraversal(one)
-#    print('Inorder traversal:')
-#    print(result)
-#    print('\n')
-#    result = s.postorderTraversal(one)
-#    print('Postorder traversal:')
-#    print(result)
-#    print('\n')
+def test_preorderTraversal_2():
+    """
+    Input: [1,null,2,3]
+    Output: [1,2,3]
+    """
+    s = Solution()
+    three = TreeNode(3)
+    two = TreeNode(2)
+    one = TreeNode(1)
+    two.left = three
+    one.right = two
+    result = s.preorderTraversal_2(one)
+    print('Preorder traversal:')
+    print(result)
+    print('\n')
 
-#    three = TreeNode(3)
-#    nine = TreeNode(9)
-#    twenty = TreeNode(20)
-#    fifteen = TreeNode(15)
-#    seven = TreeNode(7)
-#    three.left = nine
-#    three.right = twenty
-#    twenty.left = fifteen
-#    twenty.right = seven
-#    result = s.levelOrder(three)
-#    print('Level order traversal:')
-#    print(result)
-#    print('\n')
-#    result = s.maxDepth(three)
-#    print('Maximum depth of a tree:')
-#    print(result)
-#    print('\n')
+def test_inorderTraversal():
+    """
+    Input: [1,null,2,3]
+    Output: [1,3,2]
+    """
+    s = Solution()
+    three = TreeNode(3)
+    two = TreeNode(2)
+    one = TreeNode(1)
+    two.left = three
+    one.right = two    
+    result = s.inorderTraversal(one)
+    print('Inorder traversal:')
+    print(result)
+    print('\n')
 
-#    one = TreeNode(1)
-#    twoL = TreeNode(2)
-#    twoR = TreeNode(2)
-#    threeL = TreeNode(3)
-#    threeR = TreeNode(3)
-#    fourL = TreeNode(4)
-#    fourR = TreeNode(4)
-#    one.left = twoL
-#    one.right = twoR
-#    twoL.left = threeL
-#    twoR.right = threeR
-#    twoL.right = fourL
-#    twoR.left = fourL
-#    result = s.isSymmetric(one)
-#    print('Is first tree symmetric?')
-#    print(result)
-#    print('\n')
-#    twoL.left = None
-#    twoR.left = None
-#    twoL.right = threeL
-#    twoR.right = threeR
-#    result = s.isSymmetric(one)
-#    print('Is second tree symmetric?')
-#    print(result)
-#    print('\n')
+def test_postorderTrversal():
+    """
+    Input: [1,null,2,3]
+    Output: [3,2,1]
+    """
+    s = Solution()
+    three = TreeNode(3)
+    two = TreeNode(2)
+    one = TreeNode(1)
+    two.left = three
+    one.right = two
+    result = s.postorderTraversal(one)
+    print('Postorder traversal:')
+    print(result)
+    print('\n')
 
-#    five = TreeNode(5)
-#    four_a = TreeNode(4)
-#    eight = TreeNode(8)
-#    eleven = TreeNode(11)
-#    thirteen = TreeNode(13)
-#    four_b = TreeNode(4)
-#    seven = TreeNode(7)
-#    two = TreeNode(2)
-#    one = TreeNode(1)
-#    five.left = four_a
-#    five.right = eight
-#    four_a.left = eleven
-#    eleven.left = seven
-#    eleven.right = two
-#    eight.left = thirteen
-#    eight.right = four_b
-#    four_b.right = one
-#    result = s.hasPathSum(five, 22)
-#    print('Has a tree a path with sum equal to 22?')
-#    print(result)
-#    print('\n')
+def test_levelOrder():
+    """
+    Input: [3,9,20,null,null,15,7]
+    Output: [[3],[9,20],[15,7]]
+    """
+    s = Solution()
+    three = TreeNode(3)
+    nine = TreeNode(9)
+    twenty = TreeNode(20)
+    fifteen = TreeNode(15)
+    seven = TreeNode(7)
+    three.left = nine
+    three.right = twenty
+    twenty.left = fifteen
+    twenty.right = seven
+    result = s.levelOrder(three)
+    print('Level order traversal:')
+    print(result)
+    print('\n')
 
+def test_maxDepth():
+    """
+    Input: [3,9,20,null,null,15,7]
+    Output: 3
+    """
+    s = Solution()
+    three = TreeNode(3)
+    nine = TreeNode(9)
+    twenty = TreeNode(20)
+    fifteen = TreeNode(15)
+    seven = TreeNode(7)
+    three.left = nine
+    three.right = twenty
+    twenty.left = fifteen
+    twenty.right = seven
+    result = s.maxDepth(three)
+    print('Maximum depth of a tree:')
+    print(result)
+    print('\n')
+
+def test_isSymmetric():
+    """
+    Input 1: [1,2,2,3,4,4,3]
+    Output 1: True
+    Input 2: [1,2,2,null,3,null,3]
+    Output 2: False
+    """
+    s = Solution()
+    one = TreeNode(1)
+    twoL = TreeNode(2)
+    twoR = TreeNode(2)
+    threeL = TreeNode(3)
+    threeR = TreeNode(3)
+    fourL = TreeNode(4)
+    fourR = TreeNode(4)
+    one.left = twoL
+    one.right = twoR
+    twoL.left = threeL
+    twoR.right = threeR
+    twoL.right = fourL
+    twoR.left = fourL
+    result = s.isSymmetric(one)
+    print('Is first tree symmetric?')
+    print(result)
+    print('\n')
+    twoL.left = None
+    twoR.left = None
+    twoL.right = threeL
+    twoR.right = threeR
+    result = s.isSymmetric(one)
+    print('Is second tree symmetric?')
+    print(result)
+    print('\n')
+
+def test_hasPathSum():
+    """
+    Input: [5,4,8,11,null,13,4,7,2,null,1] and 22
+    Output: True
+    """
+    s = Solution()
+    five = TreeNode(5)
+    four_a = TreeNode(4)
+    eight = TreeNode(8)
+    eleven = TreeNode(11)
+    thirteen = TreeNode(13)
+    four_b = TreeNode(4)
+    seven = TreeNode(7)
+    two = TreeNode(2)
+    one = TreeNode(1)
+    five.left = four_a
+    five.right = eight
+    four_a.left = eleven
+    eleven.left = seven
+    eleven.right = two
+    eight.left = thirteen
+    eight.right = four_b
+    four_b.right = one
+    result = s.hasPathSum(five, 22)
+    print('Has a tree a path with sum equal to 22?')
+    print(result)
+    print('\n')
+
+def test_countUnivalSubtrees():
+    """
+    Input: [5,1,5,5,5,null,5]
+    Output: 4
+    """
+    s = Solution()
     t1 = TreeNode(5)
     t2 = TreeNode(1)
     t3 = TreeNode(5)
@@ -333,3 +448,43 @@ if __name__ == '__main__':
     result = s.countUnivalSubtrees(t1)
     print('Number of uni-value subtrees = {}'.format(result))
     print('\n')
+
+def printTree(tree):
+    """
+    Input: TreeNode
+    """
+    print('root = {}'.format(tree.val))
+    if tree.left == None:
+        print('no left leave')
+    else:
+        print('left tree')
+        printTree(tree.left)
+    if tree.right == None:
+        print('no right leave')
+    else:
+        print('right tree')
+        printTree(tree.right)
+
+def test_buildTree():
+    """
+    Input: [9,3,15,20,7], [9,15,7,20,3]
+    Output: [3, 9, 20, None, None, 15, 7]
+    """
+    s = Solution()
+    inorder = [9,3,15,20,7]
+    postorder = [9,15,7,20,3]
+    tree = s.buildTree(inorder, postorder)
+    printTree(tree)
+
+if __name__ == '__main__':
+
+#    test_preorderTraversal_1()
+#    test_preorderTraversal_2()
+#    test_inorderTraversal()
+#    test_postorderTrversal()
+#    test_levelOrder()
+#    test_maxDepth()
+#    test_isSymmetric()
+#    test_hasPathSum()
+#    test_countUnivalSubtrees()
+    test_buildTree()
